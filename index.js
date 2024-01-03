f006681fa66c60f20334f52e8c83bbfcc8f438f1
Saranya Selvamani <sdsaranya29@gmail.com>
2024-01-03 16:18:01 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+function destroy()
+{
+document.getElementById('display').innerHTML= "";
+
+}
+
+function key(val){
+document.getElementById('display').innerHTML += val
+}
+
+function solve()
+{
+
+let x = document.getElementById('display').innerHTML
+
+let y = eval(x);
+
+document.getElementById('display').innerHTML = y
+
+return y
+}
+
+function back()
+{
+    let value = document.getElementById('display').innerHTML
+
+    document.getElementById('display').innerHTML  = value.slice(0,-1)
+  
+}
+
+
